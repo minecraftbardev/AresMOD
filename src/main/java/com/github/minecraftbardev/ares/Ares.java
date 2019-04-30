2c4a0677e0dd5fe8668d6bb99bc74a9a75fd3da1
Romonov <Romonov2003@outlook.com>
2019-04-30 08:02:29 +0800

    Hello world!
    Signed-off-by: Romonov <Romonov2003@outlook.com>

---
+++ src/main/java/com/github/minecraftbardev/ares/Ares.java
@@ -0,0 +1,11 @@
+package com.github.minecraftbardev.ares;
+
+import net.fabricmc.api.ModInitializer;
+
+public class Ares implements ModInitializer {
+
+    @Override
+    public void onInitialize() {
+        System.out.println("Hello, world!");
+    }
+}
